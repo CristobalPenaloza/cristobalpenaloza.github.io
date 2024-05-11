b44262d3bd13d4106e59ee5e07c8dc2ac11bbd4c
Cristóbal Peñaloza <cristobal.tolin@gmail.com>
2024-05-10 23:12:36 -0400

    Fast downloads and new workflow working.

--- z64_missing_song_detector.py
+++ z64_missing_song_detector.py
@@ -10,70 +10,83 @@ def detectSongs():
     
     with open('z64musicpacker.properties') as propertiesFile:
         properties = json.load(propertiesFile)
-        #print(properties)
-        databaseDump = ''
 
-        with open(properties['database'], 'r+') as databaseFile:
-            database = json.load(databaseFile)
-            songs = os.walk(properties['binaries'])
-            
-            # Check every single file inside the binaries folder
-            for dirpath, dirnames, filenames in songs:
-                directory = dirpath.replace(properties['binaries'], '')
+        # Pack all the files in a single zip, to provide a faster download in the web tool
+        with zipfile.ZipFile('binaries.zip', 'w', zipfile.ZIP_DEFLATED) as binariesZip:
 
-                for filename in filenames:
-                    # Only check ootrs and mmrs files
-                    if not filename.endswith('.ootrs') and not filename.endswith('.mmrs'): continue
-                    
-                    # Check if the file is in the database
-                    fullPath = os.path.join(directory, filename).replace("\\","/") 
-                    detectedInDatabase = any(x["file"] == fullPath for x in database)
-                    if detectedInDatabase: continue # <-- TODO: Re-add categories, usescustombank and usescustomsamples, for consistency
+            # Open the database, so we can modify it
+            with open(properties['database'], 'r+') as databaseFile:
+                database = json.load(databaseFile)
+                songs = os.walk(properties['binaries'])
+                
+                # Check every single file inside the binaries folder
+                for dirpath, dirnames, filenames in songs:
+                    directory = dirpath.replace(properties['binaries'], '')
 
-                    # Extract data from the file
-                    isOOTRS = filename.endswith('.ootrs')
-                    type, categories, usesCustomBank, usesCustomSamples = extractMetadataFromOOTRS(os.path.join(dirpath, filename))
-                    
-                    # If is not there, add it!
-                    print('Adding missing file: ' + fullPath)
-                    database.append({
-                        'game': directory.replace("\\","/").split('/')[-1],
-                        'song': filename.replace('.ootrs', ''),
-                        'type': type,
-                        'categories': categories,
-                        'usesCustomBank': usesCustomBank,
-                        'usesCustomSamples': usesCustomSamples,
-                        'uuid': str(uuid.uuid4()),
-                        'file': fullPath
-                    })
+                    for filename in filenames:
+                        try:
+                            # Only check ootrs and mmrs files
+                            if not filename.endswith('.ootrs') and not filename.endswith('.mmrs'): continue
+                            
+                            # Add this file to the main zip
+                            osPath = os.path.join(dirpath, filename)
+                            print('Repacking file: ' + osPath)
+                            binariesZip.write(osPath)
 
-            # Replace database with this one
-            databaseFile.seek(0)
-            json.dump(database, databaseFile, indent=2)
-            databaseFile.truncate()
+                            # Check if the file is in the database
+                            fullPath = os.path.join(directory, filename).replace("\\","/") 
+                            detectedInDatabase = any(x["file"] == fullPath for x in database)
+                            if detectedInDatabase: continue # <-- TODO: Re-add categories, usescustombank and usescustomsamples, for consistency
+
+                            # Extract data from the file
+                            isOOTRS = filename.endswith('.ootrs')
+                            type, categories, usesCustomBank, usesCustomSamples = extractMetadataFromOOTRS(os.path.join(dirpath, filename))
+                            
+                            # If is not there, add it!
+                            print('Adding missing file: ' + fullPath)
+                            database.append({
+                                'game': directory.replace("\\","/").split('/')[-1],
+                                'song': filename.replace('.ootrs', ''),
+                                'type': type,
+                                'categories': categories,
+                                'usesCustomBank': usesCustomBank,
+                                'usesCustomSamples': usesCustomSamples,
+                                'uuid': str(uuid.uuid4()),
+                                'file': fullPath
+                            })
+                        except:
+                            print("An error ocurred while processing the file " + filename)
+
+                # Replace database with this one
+                databaseFile.seek(0)
+                json.dump(database, databaseFile, indent=2)
+                databaseFile.truncate()
 
     return True
 
 
 def extractMetadataFromOOTRS(path) -> tuple[str, str, bool, bool]:
-    archive = zipfile.ZipFile(path, 'r')
-    namelist = archive.namelist()
+    try:
+        archive = zipfile.ZipFile(path, 'r')
+        namelist = archive.namelist()
 
-    for name in namelist:
-        if name.endswith('.meta'):
-            with archive.open(name) as meta_file:
-                lines = meta_file.readlines()
-                lines = [line.decode('utf8').rstrip() for line in lines]
+        for name in namelist:
+            if name.endswith('.meta'):
+                with archive.open(name) as meta_file:
+                    lines = meta_file.readlines()
+                    lines = [line.decode('utf8').rstrip() for line in lines]
 
-                # Extract the type and groups
-                seq_type = lines[2] if len(lines) >= 3 else 'bgm'
-                groups = [g.strip() for g in lines[3].split(',')] if len(lines) >= 4 else []
+                    # Extract the type and groups
+                    seq_type = lines[2] if len(lines) >= 3 else 'bgm'
+                    groups = [g.strip() for g in lines[3].split(',')] if len(lines) >= 4 else []
 
-                # Check if uses custom banks and samples
-                usesCustomBank = any(n.endswith('.zbank') for n in namelist)
-                usesCustomSamples = any(n.endswith('.zsound') for n in namelist)
+                    # Check if uses custom banks and samples
+                    usesCustomBank = any(n.endswith('.zbank') for n in namelist)
+                    usesCustomSamples = any(n.endswith('.zsound') for n in namelist)
 
-                return seq_type, groups, usesCustomBank, usesCustomSamples
+                    return seq_type, groups, usesCustomBank, usesCustomSamples
+    except:
+        print("An error ocurred while processing the file " + path)
 
 
 def main():
@@ -81,8 +94,6 @@ def main():
 
     if result: print("Process completed succesfully!")
     else: print("An error occured")
-    
-    input("Press enter to quit.")
 
 
 if __name__ == '__main__':
